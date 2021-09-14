b956c28bac52bb782fe29a328607b3ebe1319167
Jlobblet <jlobblet-github@gmx.co.uk>
2021-09-14 13:14:38 +0100

    Imperative languages have return

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,16 @@ fn main(args: Args) -> Result<()> {
 
     let args = ProcessedArgs::new(args, &mut lock)?;
 
-    let exit: Result<()>;
-
     loop {
         match lock.read_line(&mut buf) {
             Ok(0) => {
                 // nothing left in stdin
-                exit = Ok(());
-                break;
+                return Ok(());
             }
             Err(e) => {
                 let mut stderr = stderr();
                 stderr.write_all(e.to_string().as_ref())?;
-                exit = Err(anyhow::Error::from(e));
-                break;
+                return  Err(anyhow::Error::from(e));
             }
             Ok(_) => {
                 if let Some(s) = split::process_line(&args, &buf) {
@@ -43,6 +39,4 @@ fn main(args: Args) -> Result<()> {
         buf.clear();
         output.flush()?;
     }
-
-    exit
 }
