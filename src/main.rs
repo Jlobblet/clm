55f582801869f99c13250035221e5c753cd5fd0b
Jlobblet <jlobblet-github@gmx.co.uk>
2021-09-14 13:10:05 +0100

    Nicer main loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{stdin, stdout, BufRead, Write};
+use std::io::{stderr, stdin, stdout, BufRead, Write};
 
 use anyhow::Result;
 
@@ -17,10 +17,21 @@ fn main(args: Args) -> Result<()> {
 
     let args = ProcessedArgs::new(args, &mut lock)?;
 
+    let exit: Result<()>;
+
     loop {
         match lock.read_line(&mut buf) {
-            Ok(0) => break,
-            Err(_) => break,
+            Ok(0) => {
+                // nothing left in stdin
+                exit = Ok(());
+                break;
+            }
+            Err(e) => {
+                let mut stderr = stderr();
+                stderr.write_all(e.to_string().as_ref())?;
+                exit = Err(anyhow::Error::from(e));
+                break;
+            }
             Ok(_) => {
                 if let Some(s) = split::process_line(&args, &buf) {
                     output.write_all(s.as_bytes())?;
@@ -30,8 +41,8 @@ fn main(args: Args) -> Result<()> {
         };
 
         buf.clear();
+        output.flush()?;
     }
 
-    output.flush()?;
-    Ok(())
+    exit
 }
